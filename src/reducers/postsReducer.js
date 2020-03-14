73ade07721cca3e3bbbd6dbf0ec520cb27e29694
AydarF <aydar_fayzullin@yahoo.com>
2020-03-14 00:19:48 -0400

    Finish postsReducer reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,11 +1,30 @@
+import * as actions from "../actions/postsActions";
+
 export const initialState = {
   posts: [],
   loading: false,
-  hasError: false
+  hasErrors: false
 };
 
 export default function postsReducer(state = initialState, action) {
   switch (action.type) {
+    case actions.GET_POSTS:
+      return {
+        ...state,
+        loading: true
+      };
+    case actions.GET_POSTS_SUCCESS:
+      return {
+        loading: false,
+        posts: action.payload,
+        hasErrors: false
+      };
+    case actions.GET_POSTS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        hasErrors: true
+      };
     default:
       return state;
   }
