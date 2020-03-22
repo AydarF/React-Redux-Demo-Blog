9ac7baf81b76ee6ea5b9e30cd8133f20f66276aa
AydarF <aydar_fayzullin@yahoo.com>
2020-03-21 21:18:47 -0400

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import {
 import DashboardPage from "./pages/DashboardPage";
 import PostsPage from "./pages/PostsPage";
 import SinglePost from "./pages/SinglePost";
+import Navbar from "./components/Navbar.js";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={DashboardPage} />
         <Route exact path="/posts" component={PostsPage} />
